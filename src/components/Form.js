3f57f31228834d834ac3054341db370290b5136a
the-timoye <thetimoye@gmail.com>
2022-03-02 19:50:29 +0100

    chore(form): apply validations

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import PropTypes from "proptypes";
 
 export const Form = () => {
     const initialValues = {
@@ -6,22 +7,32 @@ export const Form = () => {
         apiKey: ''
     }
     const [state, setState] = useState(initialValues);
+    const [loading, setLoading] = useState(false);
     const onChange = (attribute, value) => {
         setState((prev) => ({
             ...prev,
             [attribute]: value
         }))
     }
+    const validateForm = () => {
+        if (!state.name) return false;
+        if (state.apiKey.length < 12) return false;
+        return true;
+    }
     const onSubmit = () => {
-        // e.preventDefault();
+        const canSubmit = validateForm();
+        if (canSubmit) {
+            setLoading(true);
+        }
     }
     return (
-        <form className="form" onSubmit={(e) => e.preventDefault()}>
+        <form className="form">
             <div className="form__field">
                 <label>What's Your Name</label>
                 <input
                     name={'name'}
                     type="text"
+                    disabled={loading}
                     autoComplete="off"
                     className="form__input"
                     value={state.name}
@@ -35,12 +46,24 @@ export const Form = () => {
                     name='apiKey'
                     type="text"
                     className="form__input"
+                    disabled={loading}
                     value={state.apiKey}
                     placeholder="PY-STK-TEST-009X9343n9340FFN0mjkQ"
                     onChange={(e) => onChange('apiKey', e.target.value)}
                 />
             </div>
-            <button className="button button-primary" onSubmit={onSubmit}>Explore</button>
+
+            <span className={`button ${validateForm() ? 'button-primary' : 'color-grey bg-primary-faded'}`} onClick={onSubmit}>
+                {loading ?
+                    <div className="loader">
+                        <div />
+                        <div />
+                        <div />
+                    </div> : 'Explore'}
+            </span>
         </form>
     )
+}
+Form.propTypes = {
+    startApiCall: PropTypes.func
 }
