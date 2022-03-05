ed47bafadbb9c4f9e260861177da383f6c0573e2
the-timoye <thetimoye@gmail.com>
2022-03-05 19:30:06 +0100

    chore(dashboard): accomodate diff. dashboard layouts

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,15 +1,13 @@
 import React from "react";
 import { MainLayout } from "../layouts/dashboard/main";
+import { Overview } from "../layouts/dashboard/overview";
 import { Sidebar } from "../layouts/dashboard/sidebar";
 
 const Dashboard = () => {
     return (
         <div className="dashboard">
             <Sidebar />
-            <MainLayout
-                name={"Timi"}
-                subtitle={"See all payments made by your customers"}
-            />
+            <Overview Layout={MainLayout} />
         </div>
     )
 }
