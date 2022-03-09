9d33e2c4574e37db6964406b9c48737916826b3b
the-timoye <thetimoye@gmail.com>
2022-03-09 20:34:51 +0100

    feat(dashboard): implement listing screen

--- src/layouts/dashboard/lists.js
+++ src/layouts/dashboard/lists.js
@@ -2,32 +2,116 @@ import React from "react";
 import { MainLayout } from "./main";
 
 export const Lists = () => {
-    const cardItems = [
-        { title: "total subscribers", count: 600 },
-        { title: "amount earned", count: 8000, currency: "$" },
-        { title: "total payments", count: 7000 }
-    ]
     return (
         <MainLayout name="Timi"
             subtitle={"See all payments made by your customers"}
             showSections
         >
-            <div className="lists">
-                <div className="lists__cards-container">
-                    {cardItems.map(({ title, count, currency }) => {
-                        return (
-                            <div className="lists__cards-container__card">
-                                <p className="title">{title}</p>
-                                <p className="count"> {currency && currency}{count}</p>
-                            </div>
-                        )
-                    })}
-                </div>
-                <div className="lists__charts">
-                    <div className="lists__charts-main"></div>
-
-                    <div className="lists__charts-sub"></div>
-                </div>
+            <div className="lists table">
+                <table>
+                    <thead>
+                        <th>S/N</th>
+                        <th>Name</th>
+                        <th>Phone</th>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>Adebambo Shonekun</td>
+                            <td>+2348099887744</td>
+                        </tr>
+                        <tr>
+                            <td>2</td>
+                            <td>Adebambo Shonekun</td>
+                            <td>+2348099887744</td>
+                        </tr>
+                        <tr>
+                            <td>3</td>
+                            <td>Adebambo Shonekun</td>
+                            <td>+2348099887744</td>
+                        </tr>
+                        <tr>
+                            <td>3</td>
+                            <td>Adebambo Shonekun</td>
+                            <td>+2348099887744</td>
+                        </tr>
+                        <tr>
+                            <td>3</td>
+                            <td>Adebambo Shonekun</td>
+                            <td>+2348099887744</td>
+                        </tr>
+                        <tr>
+                            <td>3</td>
+                            <td>Adebambo Shonekun</td>
+                            <td>+2348099887744</td>
+                        </tr>
+                        <tr>
+                            <td>3</td>
+                            <td>Adebambo Shonekun</td>
+                            <td>+2348099887744</td>
+                        </tr>
+                        <tr>
+                            <td>3</td>
+                            <td>Adebambo Shonekun</td>
+                            <td>+2348099887744</td>
+                        </tr>
+                        <tr>
+                            <td>3</td>
+                            <td>Adebambo Shonekun</td>
+                            <td>+2348099887744</td>
+                        </tr>
+                        <tr>
+                            <td>3</td>
+                            <td>Adebambo Shonekun</td>
+                            <td>+2348099887744</td>
+                        </tr>
+                        <tr>
+                            <td>3</td>
+                            <td>Adebambo Shonekun</td>
+                            <td>+2348099887744</td>
+                        </tr>
+                        <tr>
+                            <td>3</td>
+                            <td>Adebambo Shonekun</td>
+                            <td>+2348099887744</td>
+                        </tr>
+                        <tr>
+                            <td>3</td>
+                            <td>Adebambo Shonekun</td>
+                            <td>+2348099887744</td>
+                        </tr>
+                        <tr>
+                            <td>3</td>
+                            <td>Adebambo Shonekun</td>
+                            <td>+2348099887744</td>
+                        </tr>
+                        <tr>
+                            <td>3</td>
+                            <td>Adebambo Shonekun</td>
+                            <td>+2348099887744</td>
+                        </tr>
+                        <tr>
+                            <td>3</td>
+                            <td>Adebambo Shonekun</td>
+                            <td>+2348099887744</td>
+                        </tr>
+                        <tr>
+                            <td>3</td>
+                            <td>Adebambo Shonekun</td>
+                            <td>+2348099887744</td>
+                        </tr>
+                        <tr>
+                            <td>3</td>
+                            <td>Adebambo Shonekun</td>
+                            <td>+2348099887744</td>
+                        </tr>
+                        <tr>
+                            <td>3</td>
+                            <td>Adebambo Shonekun</td>
+                            <td>+2348099887744</td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </MainLayout>
 
